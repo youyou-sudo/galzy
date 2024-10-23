07f6bb1812a2690cf8543d48d1bc31f8323ca2ec
小忧忧 <60727068+youyou-sudo@users.noreply.github.com>
2024-10-23 10:21:59 +0800

    bug: 修复点击 Link 后 File Modal 被全部打开的问题

--- app/[vnid]/(components)/Datalistview.tsx
+++ app/[vnid]/(components)/Datalistview.tsx
@@ -69,6 +69,65 @@ const pathColorMap = {
   ),
 };
 
+function Modalfun({
+  isOpen,
+  onOpenChange,
+  data,
+  dlink,
+  gfpath,
+  pathtmp,
+}: {
+  isOpen: boolean;
+  onOpenChange: (open: boolean) => void;
+  data: any;
+  dlink: string;
+  gfpath: string;
+  pathtmp?: string;
+}) {
+  if (!data) return null;
+
+  // Size 转换
+  function formatBytes(bytes: number, decimals: number = 2): string {
+    if (bytes === 0) return "0 字节";
+    const k = 1024;
+    const dm = decimals < 0 ? 0 : decimals;
+    const sizes = ["字节", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"];
+
+    const i = Math.floor(Math.log(bytes) / Math.log(k));
+
+    return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + " " + sizes[i];
+  }
+
+  return (
+    <Modal isOpen={isOpen} onOpenChange={onOpenChange} placement="center">
+      <ModalContent>
+        <>
+          <ModalHeader className="flex flex-col gap-1">文件详情</ModalHeader>
+          <ModalBody>
+            <div className="flex flex-col gap-2">
+              <p className="text-center text-lg">{data.name}</p>
+              <p className="text-center">{formatBytes(data.size)}</p>
+            </div>
+          </ModalBody>
+          <ModalFooter className="flex justify-center gap-4">
+            <Button variant="secondary" onClick={() => onOpenChange(false)}>
+              关闭
+            </Button>
+            <Button asChild>
+              <Link
+                target="_blank"
+                href={`${dlink}/${gfpath}/${pathtmp}/${data.name}?sign=${data.sign}`}
+              >
+                下载
+              </Link>
+            </Button>
+          </ModalFooter>
+        </>
+      </ModalContent>
+    </Modal>
+  );
+}
+
 // 文件列递归组件
 const FileMap = ({
   filelist,
@@ -83,115 +142,89 @@ const FileMap = ({
   dlink?: string;
   gfpath: string;
 }) => {
-  const { isOpen, onOpen, onOpenChange } = useDisclosure();
   if (!filelist || filelist.length === 0) return null;
-  function formatBytes(bytes: number, decimals: number = 2): string {
-    if (bytes === 0) return "0 字节";
-    const k = 1024;
-    const dm = decimals < 0 ? 0 : decimals;
-    const sizes = ["字节", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"];
 
-    const i = Math.floor(Math.log(bytes) / Math.log(k));
+  // Modal 控制
+  const [selectedFile, setSelectedFile] = useState<any>(null);
+  const { isOpen, onOpen, onOpenChange } = useDisclosure();
 
-    return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + " " + sizes[i];
-  }
-
+  const handleFileClick = (file: any) => {
+    setSelectedFile(file);
+    onOpen();
+  };
   return (
-    <motion.div
-      className="flex flex-col"
-      initial={{ opacity: 0, y: -5 }}
-      animate={{ opacity: 1, y: 0 }}
-      transition={{ delay: 0.1 }}
-    >
-      <ul style={{ marginLeft: `${level > 0 ? 25 : 0}px` }}>
-        {filelist.map((item, index) => (
-          <motion.div
-            key={index}
-            className="flex flex-col"
-            initial={{ opacity: 0, y: -5 }}
-            animate={{ opacity: 1, y: 0 }}
-            transition={{
-              delay: index * 0.02,
-              duration: 0.1,
-            }}
-          >
-            <li>
-              {item.is_dir === true ? (
-                <>
-                  {pathColorMap[item.name] ? (
-                    <div className="flex text-2xl font-bold gap-2">
-                      {pathColorMap[item.name]}
-                    </div>
-                  ) : (
-                    <>
-                      <p>{item.name}</p>
-                      <Divider />
-                    </>
-                  )}
-                  <FileMap
-                    filelist={item.filelist}
-                    level={level + 1}
-                    pathtmp={pathtmp || "" + "/" + item.name}
-                    dlink={dlink}
-                    gfpath={gfpath}
-                  />
-                </>
-              ) : (
-                <div className="list-disc flex">
-                  <Button
-                    // as={Button}
-                    variant="link"
-                    onClick={onOpen}
-                    className="block truncate"
-                  >
-                    {item.name}
-                    <span className="inline-flex items-center">
-                      <MdOutlinePageview />
-                    </span>
-                  </Button>
-                  <Modal
-                    isOpen={isOpen}
-                    onOpenChange={onOpenChange}
-                    placement="center"
-                  >
-                    <ModalContent>
-                      {(onClose) => (
-                        <>
-                          <ModalHeader className="flex flex-col gap-1">
-                            文件详情
-                          </ModalHeader>
-                          <ModalBody>
-                            <div className="flex flex-col gap-2">
-                              <p className="text-center text-lg">{item.name}</p>
-                              <p className="text-center">
-                                {formatBytes(item.size)}
-                              </p>
-                            </div>
-                          </ModalBody>
-                          <ModalFooter className="flex justify-center gap-4">
-                            <Button variant="secondary" onClick={onClose}>
-                              关闭
-                            </Button>
-                            <Button asChild>
-                              <Link
-                                target="_blank"
-                                href={`${dlink}/${gfpath}/${pathtmp}/${item.name}?sign=${item.sign}`}
-                              >
-                                下载
-                              </Link>
-                            </Button>
-                          </ModalFooter>
-                        </>
-                      )}
-                    </ModalContent>
-                  </Modal>
-                </div>
-              )}
-            </li>
-          </motion.div>
-        ))}
-      </ul>
-    </motion.div>
+    <>
+      <motion.div
+        className="flex flex-col"
+        initial={{ opacity: 0, y: -5 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ delay: 0.1 }}
+      >
+        <ul style={{ marginLeft: `${level > 0 ? 25 : 0}px` }}>
+          {filelist.map((item, index) => (
+            <motion.div
+              key={index}
+              className="flex flex-col"
+              initial={{ opacity: 0, y: -5 }}
+              animate={{ opacity: 1, y: 0 }}
+              transition={{
+                delay: index * 0.02,
+                duration: 0.1,
+              }}
+            >
+              <li>
+                {item.is_dir === true ? (
+                  <>
+                    {pathColorMap[item.name] ? (
+                      <div className="flex text-2xl font-bold gap-2">
+                        {pathColorMap[item.name]}
+                      </div>
+                    ) : (
+                      <>
+                        <p>{item.name}</p>
+                        <Divider />
+                      </>
+                    )}
+                    <FileMap
+                      filelist={item.filelist}
+                      level={level + 1}
+                      pathtmp={pathtmp || "" + "/" + item.name}
+                      dlink={dlink}
+                      gfpath={gfpath}
+                    />
+                  </>
+                ) : (
+                  <div className="list-disc flex">
+                    <Button
+                      // as={Button}
+                      variant="link"
+                      onClick={() => {
+                        handleFileClick(item);
+                      }}
+                      className="block truncate"
+                    >
+                      {item.name}
+                      <span className="inline-flex items-center">
+                        <MdOutlinePageview />
+                      </span>
+                    </Button>
+                  </div>
+                )}
+              </li>
+            </motion.div>
+          ))}
+        </ul>
+      </motion.div>
+
+      <Modalfun
+        isOpen={isOpen}
+        onOpenChange={onOpenChange}
+        data={selectedFile}
+        dlink={dlink}
+        gfpath={gfpath}
+        pathtmp={pathtmp}
+      />
+    </>
   );
 };
 
