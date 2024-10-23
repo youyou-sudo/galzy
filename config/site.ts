df4ac5d4166321e4bcd7427b3fa571b6386b762f
小忧忧 <60727068+youyou-sudo@users.noreply.github.com>
2024-10-23 12:00:11 +0800

    fix: 修改为正确项目名和 SEO 优化

--- config/site.ts
+++ config/site.ts
@@ -1,7 +1,7 @@
 export type SiteConfig = typeof siteConfig;
 
 export const siteConfig = {
-  name: "GnGame",
+  name: "VNGet",
   description:
     "又又又一个视觉小说资源站喵～；收录各种恋爱养成游戏、美少女游戏(ACGgame, Galgame)、二次元视觉小说等ACG领域资源的类别。提供游戏简介，讨论，资源下载。",
   keywords: [
@@ -10,10 +10,31 @@ export const siteConfig = {
     "二次元视觉小说",
     "ギャルゲーム",
     "ACGgame",
-    "Gal",
-    " Visual Novel",
+    "Visual Novel",
     "视觉小说",
-    "Game",
+    "VNGet",
+    "Visual Novel Download",
+    "Galgame Download",
+    "ACGgame Download",
+    "Game Download",
+    "Galgame 下载",
+    "视觉小说 下载",
+    "二次元视觉小说 下载",
+    "ギャルゲーム 下载",
+    "Galgame 免费下载",
+    "新作 Galgame",
+    "经典 Visual Novel",
+    "热门 Galgame",
+    "冷门 Galgame",
+    "PC版 Galgame",
+    "iOS Visual Novel 下载",
+    "安卓 Visual Novel 下载",
+    "Kirikiri Galgame",
+    "Tyranor Galgame",
+    "ONScripter Galgame",
+    "Ren'Py Galgame",
+    "汉化版 Visual Novel",
+    "同人 Galgame",
   ],
   navItems: [
     {
