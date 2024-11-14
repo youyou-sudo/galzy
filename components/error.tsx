396748e2c6279c45189b884d9e5e34367d81fcb9
小忧忧 <60727068+youyou-sudo@users.noreply.github.com>
2024-11-14 16:59:04 +0800

    docs: 添加 Error 组件的类型文档注释

--- components/error.tsx
+++ components/error.tsx
@@ -2,7 +2,20 @@
 
 import { Image } from "@nextui-org/react";
 
-export default function Errors({ code }) {
+interface ErrorsProps {
+  code: string;
+}
+
+/**
+ * `Errors` 组件，用于显示错误信息
+ * @param {string} props.code - 错误代码
+ *
+ * @example
+ * ```tsx
+ * <Errors code="404" />
+ * ```
+ */
+export default function Errors({ code }: ErrorsProps) {
   return (
     <div className="flex flex-col">
       <div className="flex items-center justify-center">
