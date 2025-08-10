079a231085b273046c217ee5e71e99fc3e820798
youyou <60727068+youyou-sudo@users.noreply.github.com>
2025-08-11 00:51:30 +0800

    fix: 将索引放入环境变量以确保安全

--- components/Search/meilisearch.tsx
+++ components/Search/meilisearch.tsx
@@ -1,8 +1,11 @@
 import MeiliSearchClient from "@/lib/meilisearch";
+import { env } from "next-runtime-env";
 import React from "react";
 
 const MeilisearchCard = async ({}) => {
-  const search = await MeiliSearchClient.index("steam-videogames").search("");
+  const search = await MeiliSearchClient.index(
+    env("MEILISEARCH_INDEXNAME")!
+  ).search("");
   return (
     <code>
       <pre>{JSON.stringify(search, null, 2)}</pre>
