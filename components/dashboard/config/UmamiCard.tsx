1fdcc92ae9a9574220b07d97da7a279b048caeb3
youyou <60727068+youyou-sudo@users.noreply.github.com>
2025-08-15 00:48:22 +0800

    fix: 更新一些组件、meilisearch 环境变量、Umami 后台数据服务器加载

--- components/dashboard/config/UmamiCard.tsx
+++ components/dashboard/config/UmamiCard.tsx
@@ -7,19 +7,35 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 import { UmamiCardInput } from "./UmamiCard-Input";
+import {
+  dehydrate,
+  HydrationBoundary,
+  QueryClient,
+} from "@tanstack/react-query";
+import { umamiConfigGet } from "@/lib/dashboard/config/UmamiFormAc";
 
 export function UmamiCard() {
+  const queryClient = new QueryClient();
+  queryClient.prefetchQuery({
+    queryKey: ["umamiConfig"],
+    queryFn: async () => {
+      const response = await umamiConfigGet();
+      return response;
+    },
+  });
   return (
     <div>
-      <Card>
-        <CardHeader>
-          <CardTitle>Umami 配置</CardTitle>
-          <CardDescription> Umami </CardDescription>
-        </CardHeader>
-        <CardContent>
-          <UmamiCardInput />
-        </CardContent>
-      </Card>
+      <HydrationBoundary state={dehydrate(queryClient)}>
+        <Card>
+          <CardHeader>
+            <CardTitle>Umami 配置</CardTitle>
+            <CardDescription> Umami </CardDescription>
+          </CardHeader>
+          <CardContent>
+            <UmamiCardInput />
+          </CardContent>
+        </Card>
+      </HydrationBoundary>
     </div>
   );
 }
