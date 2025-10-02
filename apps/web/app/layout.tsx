bc4ab0e48d853c22792a5cc7f2f2f0508d3615bc
youyou <60727068+youyou-sudo@users.noreply.github.com>
2025-10-02 22:30:21 +0800

    fix: 修复 metadataBase 部分域名

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -5,6 +5,12 @@ import { ThemeProvider } from '@web/components/provider/theme-provider'
 import { PublicEnvScript } from 'next-runtime-env'
 import { ViewTransitions } from 'next-view-transitions'
 import { NuqsAdapter } from 'nuqs/adapters/next/app'
+import type { Metadata } from 'next'
+import 'lib/env'
+
+export const metadata: Metadata = {
+  metadataBase: new URL(process.env.BETTER_AUTH_URL!),
+}
 
 export default function RootLayout({
   children,
