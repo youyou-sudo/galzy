a91ec521340b132d41fd99f8862fb1acfa69bb78
youyou <60727068+youyou-sudo@users.noreply.github.com>
2025-10-31 01:19:39 +0800

    fix: 回滚 e8f2c20

--- apps/api/src/modules/cron/index.ts
+++ apps/api/src/modules/cron/index.ts
@@ -3,46 +3,46 @@ import { Elysia } from 'elysia'
 import { CronService } from './service'
 
 export const cronServer = new Elysia()
-  // .use(
-  //   cron({
-  //     name: 'workerDataPull',
-  //     pattern: Patterns.everyMinutes(1),
-  //     run() {
-  //       CronService.workerDataPull()
-  //       console.log('workerDataPull')
-  //     },
-  //   })
-  // )
-  // .use(
-  //   cron({
-  //     name: 'alistSyncScript',
-  //     pattern: Patterns.everyMinutes(5),
-  //     run() {
-  //       CronService.alistSyncScript()
-  //       console.log('alistSyncScript')
-  //     },
-  //   })
-  // )
-  // .use(
-  //   cron({
-  //     name: 'meiliSearchAddIndex',
-  //     pattern: Patterns.everyHours(12),
-  //     run() {
-  //       CronService.meiliSearchAddIndex()
-  //       console.log('meiliSearchAddIndex')
-  //     },
-  //   })
-  // )
-  // .use(
-  //   cron({
-  //     name: 'meiliSearchAddTag',
-  //     pattern: Patterns.everyHours(12),
-  //     run() {
-  //       CronService.meiliSearchAddTag()
-  //       console.log('meiliSearchAddTag')
-  //     },
-  //   })
-  // )
+  .use(
+    cron({
+      name: 'workerDataPull',
+      pattern: Patterns.everyMinutes(1),
+      run() {
+        CronService.workerDataPull()
+        console.log('workerDataPull')
+      },
+    })
+  )
+  .use(
+    cron({
+      name: 'alistSyncScript',
+      pattern: Patterns.everyMinutes(5),
+      run() {
+        CronService.alistSyncScript()
+        console.log('alistSyncScript')
+      },
+    })
+  )
+  .use(
+    cron({
+      name: 'meiliSearchAddIndex',
+      pattern: Patterns.everyHours(12),
+      run() {
+        CronService.meiliSearchAddIndex()
+        console.log('meiliSearchAddIndex')
+      },
+    })
+  )
+  .use(
+    cron({
+      name: 'meiliSearchAddTag',
+      pattern: Patterns.everyHours(12),
+      run() {
+        CronService.meiliSearchAddTag()
+        console.log('meiliSearchAddTag')
+      },
+    })
+  )
   .get('/task/meiliSearchAddIndex', () => CronService.meiliSearchAddIndex())
   .get('/task/meiliSearchAddTag', () => CronService.meiliSearchAddTag())
   .get('/task/alistSyncScript', () => CronService.alistSyncScript())
