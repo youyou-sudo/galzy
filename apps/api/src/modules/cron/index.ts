e8f2c20e7ef2c69f056b49707f53016efb266713
youyou <60727068+youyou-sudo@users.noreply.github.com>
2025-10-31 00:56:47 +0800

    fix(build): 暂时跳过 cron 代码以成功构建 web，后续回滚

--- apps/api/src/modules/cron/index.ts
+++ apps/api/src/modules/cron/index.ts
@@ -2,61 +2,48 @@ import { cron, Patterns } from '@elysiajs/cron'
 import { Elysia } from 'elysia'
 import { CronService } from './service'
 
-export const cronServer: any = new Elysia()
-  .use(
-    cron({
-      name: 'workerDataPull',
-      pattern: Patterns.everyMinutes(1),
-      run() {
-        CronService.workerDataPull()
-        console.log('workerDataPull')
-      },
-    }),
-  )
-  .use(
-    cron({
-      name: 'alistSyncScript',
-      pattern: Patterns.everyMinutes(5),
-      run() {
-        CronService.alistSyncScript()
-        console.log('alistSyncScript')
-      },
-    }),
-  )
-  .use(
-    cron({
-      name: 'meiliSearchAddIndex',
-      pattern: Patterns.everyHours(12),
-      run() {
-        CronService.meiliSearchAddIndex()
-        console.log('meiliSearchAddIndex')
-      },
-    }),
-  )
-  .use(
-    cron({
-      name: 'meiliSearchAddTag',
-      pattern: Patterns.everyHours(12),
-      run() {
-        CronService.meiliSearchAddTag()
-        console.log('meiliSearchAddTag')
-      },
-    }),
-  )
-  .get('/task/meiliSearchAddIndex', () => {
-    console.log('meiliSearchAddIndex')
-    return CronService.meiliSearchAddIndex()
-  })
-  .get('/task/meiliSearchAddTag', () => {
-    console.log('meiliSearchAddTag')
-    return CronService.meiliSearchAddTag()
-  })
-
-  .get('/task/alistSyncScript', () => {
-    console.log('alistSyncScript')
-    return CronService.alistSyncScript()
-  })
-  .get('/task/workerDataPull', () => {
-    console.log('workerDataPull')
-    return CronService.workerDataPull()
-  })
+export const cronServer = new Elysia()
+  // .use(
+  //   cron({
+  //     name: 'workerDataPull',
+  //     pattern: Patterns.everyMinutes(1),
+  //     run() {
+  //       CronService.workerDataPull()
+  //       console.log('workerDataPull')
+  //     },
+  //   })
+  // )
+  // .use(
+  //   cron({
+  //     name: 'alistSyncScript',
+  //     pattern: Patterns.everyMinutes(5),
+  //     run() {
+  //       CronService.alistSyncScript()
+  //       console.log('alistSyncScript')
+  //     },
+  //   })
+  // )
+  // .use(
+  //   cron({
+  //     name: 'meiliSearchAddIndex',
+  //     pattern: Patterns.everyHours(12),
+  //     run() {
+  //       CronService.meiliSearchAddIndex()
+  //       console.log('meiliSearchAddIndex')
+  //     },
+  //   })
+  // )
+  // .use(
+  //   cron({
+  //     name: 'meiliSearchAddTag',
+  //     pattern: Patterns.everyHours(12),
+  //     run() {
+  //       CronService.meiliSearchAddTag()
+  //       console.log('meiliSearchAddTag')
+  //     },
+  //   })
+  // )
+  .get('/task/meiliSearchAddIndex', () => CronService.meiliSearchAddIndex())
+  .get('/task/meiliSearchAddTag', () => CronService.meiliSearchAddTag())
+  .get('/task/alistSyncScript', () => CronService.alistSyncScript())
+  .get('/task/workerDataPull', () => CronService.workerDataPull())
