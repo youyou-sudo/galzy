0a8019abc0aaddc49d2b042ed528ea7911c6cbca
youyou <60727068+youyou-sudo@users.noreply.github.com>
2025-11-04 12:30:03 +0800

    fix: 修复 auth origin

--- apps/web/lib/auth-client.ts
+++ apps/web/lib/auth-client.ts
@@ -1,7 +1,9 @@
 import { adminClient } from 'better-auth/client/plugins'
 import { createAuthClient } from 'better-auth/react' // make sure to import from better-auth/react sure to import from better-auth/react
+import '@web/lib/env'
 
 export const authClient = createAuthClient({
   //you can pass client configuration here
   plugins: [adminClient()],
+  origin: `${process.env.API_HOST}:${process.env.API_PORT}`,
 })
