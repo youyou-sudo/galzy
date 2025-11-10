20b18818186c31439d709c412ba17acb16b143f4
youyou <60727068+youyou-sudo@users.noreply.github.com>
2025-11-11 01:26:03 +0800

    fix: yuqu

--- apps/web/app/(app)/(home)/(components)/remf.tsx
+++ apps/web/app/(app)/(home)/(components)/remf.tsx
@@ -18,7 +18,7 @@ export default async function RankingList<T extends Item>({
   const datas = await fetchData()
 
   return (
-    <ul className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2 max-h-[9rem] overflow-y-auto">
+    <ul className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2 max-h-36 overflow-y-auto">
       {datas.map((item, index) => {
         const linkValue = item[linkKey]
         if (!linkValue) return null
@@ -29,7 +29,7 @@ export default async function RankingList<T extends Item>({
             <Link href={href} prefetch={false}  className="block w-full">
               <span className="flex items-center w-full">
                 <span
-                  className={`font-bold mr-2 flex-shrink-0 ${
+                  className={`font-bold mr-2 shrink-0 ${
                     index === 0
                       ? 'text-red-500'
                       : index === 1
