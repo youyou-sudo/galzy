938d8a7e26cf2207c20d4c661814b739f5983c85
youyou <60727068+youyou-sudo@users.noreply.github.com>
2025-11-23 17:02:32 +0800

    fix: 释放 action 传输大小为 5mb 以解决 tag

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -7,6 +7,9 @@ const nextConfig: NextConfig = {
   reactCompiler: true,
   experimental: {
     webpackMemoryOptimizations: true,
+    serverActions: {
+      bodySizeLimit: '5mb',
+    },
   },
   images: {
     unoptimized: true,
