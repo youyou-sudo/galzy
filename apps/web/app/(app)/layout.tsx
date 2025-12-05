9f6742a09d7e0d899634e085195fab4430a6768a
youyou <60727068+youyou-sudo@users.noreply.github.com>
2025-12-05 12:17:17 +0800

    chore: 首页广告 Banner

--- apps/web/app/(app)/layout.tsx
+++ apps/web/app/(app)/layout.tsx
@@ -1,9 +1,12 @@
 import { Footer } from '@web/components/footer'
 import { Navbar } from '@web/components/navbar'
+import { AspectRatio } from '@web/components/ui/aspect-ratio'
 import { Toaster } from '@web/components/ui/sonner'
 import UmamiScript from '@web/components/umami/script'
 import { metadataConfig } from '@web/config/metadata'
 import type { Metadata } from 'next'
+import Image from 'next/image'
+import Link from 'next/link'
 
 export const metadata: Metadata = {
   title: {
@@ -29,13 +32,13 @@ export default async function YoyoLayout({
       <div className="flex-1 px-3 pt-2">
         <Navbar />
         {/* 广告 */}
-        {/* <aside id="sidebar-ad" className="flex flex-col mx-auto lg:px-24 max-w-7xl px-4 py-0 mt-2 opacity-80 relative">
+        <aside id="sidebar-ad" className="flex flex-col mx-auto lg:px-24 max-w-7xl px-4 py-0 mt-2 opacity-80 relative">
           <Link
             data-umami-event="广告点击"
             data-umami-event-name="dzmm"
             data-umami-event-position="Banner-dzmm"
             target="_blank"
-            href="https://www.电子魅魔.com/?rf=876926e5"
+            href="https://www.xn--i8s951di30azba.com?rf=876926e5"
           >
             <div id="sidebar-ad" className="sm:hidden">
               <AspectRatio ratio={80 / 9}>
@@ -58,7 +61,7 @@ export default async function YoyoLayout({
               </AspectRatio>
             </div>
           </Link>
-        </aside> */}
+        </aside>
         <div className="flex flex-col mx-auto max-w-7xl space-y-4 py-4 lg:px-0">
           <main>{children}</main>
           <Toaster position="top-center" />
