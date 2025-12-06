38b8f255b68c8377d3eb90eefc16c33eb5a9f404
youyou <60727068+youyou-sudo@users.noreply.github.com>
2025-12-06 17:52:37 +0800

    fix: 修复构建错误

--- apps/web/lib/auth/auth-server.ts
+++ apps/web/lib/auth/auth-server.ts
@@ -1,22 +1,22 @@
 import 'server-only'
+import 'lib/env'
 import { createAuthClient } from 'better-auth/client'
 import { adminClient } from 'better-auth/client/plugins'
 import { headers } from 'next/headers'
-import {
-  BETTER_AUTH_BASE_PATH,
-  BETTER_AUTH_COOKIE_PREFIX,
-  BETTER_AUTH_URL,
-} from './config'
 
+
+export const BETTER_AUTH_URL = process.env.API_HOST ? `${process.env.API_HOST}:${process.env.API_PORT}` : 'http://localhost:3001'
+export const BETTER_AUTH_BASE_PATH = '/auth'
+export const BETTER_AUTH_COOKIE_PREFIX = 'better-auth' // <- default prefix
 /**
  * Better Auth client for Server-side
  */
 export const authServerClient = createAuthClient({
   plugins: [adminClient()],
-  baseURL: BETTER_AUTH_URL || 'http://localhost:3001',
+  baseURL: BETTER_AUTH_URL,
   basePath: BETTER_AUTH_BASE_PATH,
   fetchOptions: {
-    baseURL: (BETTER_AUTH_URL || 'http://localhost:3001') + BETTER_AUTH_BASE_PATH,
+    baseURL: BETTER_AUTH_URL + BETTER_AUTH_BASE_PATH,
     onRequest: async (context) => {
       const headersList = await headers()
       const cookie = headersList.get('Cookie')
--- apps/web/lib/auth/config.ts
+++
@@ -1,5 +0,0 @@
-import 'lib/env'
-
-export const BETTER_AUTH_URL = `${process.env.API_HOST}:${process.env.API_PORT}`
-export const BETTER_AUTH_BASE_PATH = '/auth'
-export const BETTER_AUTH_COOKIE_PREFIX = 'better-auth' // <- default prefix
