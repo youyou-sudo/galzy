9f1a0bdeb191585403237b600b067fa59ce98836
youyou <60727068+youyou-sudo@users.noreply.github.com>
2025-12-06 18:08:19 +0800

    chore: 开发环境下不触发 umami 统计

--- apps/web/components/umami/script.tsx
+++ apps/web/components/umami/script.tsx
@@ -2,11 +2,17 @@ import Script from 'next/script'
 import '@web/lib/env'
 
 export default async function UmamiScript() {
+  if (process.env.NODE_ENV !== 'production') return null
+
+  const umami_script_url = process.env.UMAMI_URL
+  const umami_website_id = process.env.UMAMI_DATA_WEBSITE_ID
+  if (!umami_script_url || !umami_website_id) return null
+
   return (
     <Script
-      src={`${process.env.UMAMI_URL}/script.js`}
-      data-website-id={`${process.env.UMAMI_DATA_WEBSITE_ID}`}
       defer
+      src={umami_script_url}
+      data-website-id={umami_website_id}
     />
   )
 }
