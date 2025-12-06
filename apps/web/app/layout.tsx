1d87f6995eacbc340df54bd8a798e5652a4fc192
youyou <60727068+youyou-sudo@users.noreply.github.com>
2025-12-06 18:06:20 +0800

    chore: 添加字体

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -5,6 +5,34 @@ import { ThemeProvider } from '@web/components/provider/theme-provider'
 import type { Metadata } from 'next'
 import { PublicEnvScript } from 'next-runtime-env'
 import { NuqsAdapter } from 'nuqs/adapters/next/app'
+import { Cinzel, Noto_Sans, Noto_Sans_JP, Noto_Sans_Mono, Noto_Sans_SC } from 'next/font/google'
+
+const cinzel = Cinzel({
+  subsets: ['latin'],
+  weight: ['400', '500', '600', '700', '800', '900'],
+  display: 'swap',
+  variable: '--font-cinzel',
+})
+const notoSans_Latin = Noto_Sans({
+  display: 'swap',
+  variable: '--font-latin',
+  subsets: ['latin'],
+})
+const notoSans_SC = Noto_Sans_SC({
+  display: 'swap',
+  variable: '--font-sc',
+  subsets: ['latin'],
+})
+const notoSans_JP = Noto_Sans_JP({
+  display: 'swap',
+  variable: '--font-jp',
+  subsets: ['latin'],
+})
+const notoSans_Mono = Noto_Sans_Mono({
+  display: 'swap',
+  variable: '--font-mono',
+  subsets: ['latin'],
+})
 
 export const metadata: Metadata = {
   metadataBase: new URL('https://galzy.eu.org'),
@@ -26,7 +54,11 @@ export default function RootLayout({
     },
   }
   return (
-    <html lang="zh-CN" suppressHydrationWarning>
+    <html
+      lang="zh-CN"
+      className={`${notoSans_Latin.variable} ${notoSans_SC.variable} ${notoSans_JP.variable} ${notoSans_Mono.variable} ${cinzel.variable}`}
+      suppressHydrationWarning
+    >
       <head>
         <PublicEnvScript />
         <script
