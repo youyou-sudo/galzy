4195245b14ec53048739f0bde0b4a09b1390c535
youyou <60727068+youyou-sudo@users.noreply.github.com>
2025-12-09 18:16:16 +0800

    fix: umami 统计脚本修复

--- apps/web/components/umami/script.tsx
+++ apps/web/components/umami/script.tsx
@@ -1,14 +1,14 @@
 import Script from 'next/script'
 import '@web/lib/env'
 
-export default async function UmamiScript() {
+export default function UmamiScript() {
   if (process.env.NODE_ENV !== 'production') return null
 
-  const umami_script_url = process.env.UMAMI_URL
+  const umami_script_url = process.env.UMAMI_SCRIPT_URL
   const umami_website_id = process.env.UMAMI_DATA_WEBSITE_ID
   if (!umami_script_url || !umami_website_id) return null
 
   return (
-    <Script defer src={umami_script_url} data-website-id={umami_website_id} />
+    <Script defer src={`${umami_script_url}`} data-website-id={umami_website_id} />
   )
 }
