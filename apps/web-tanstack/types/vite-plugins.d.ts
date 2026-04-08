b6bff01748ddb97a8be2d654ef0179b851a213a6
小忧忧 <60727068+youyou-sudo@users.noreply.github.com>
2026-04-08 14:26:06 +0800

    fix: 更新依赖项，替换 vite-tsconfig-paths 为 vite-plugin-babel，并添加类型声明文件

---
+++ apps/web-tanstack/types/vite-plugins.d.ts
@@ -0,0 +1,2 @@
+declare module "@tailwindcss/vite";
+declare module "@tanstack/devtools-vite";
--- apps/web-tanstack/vite.config.ts
+++ apps/web-tanstack/vite.config.ts
@@ -1,46 +1,55 @@
+import { defineConfig, loadEnv } from "vite";
+import babel from 'vite-plugin-babel';
 import tailwindcss from "@tailwindcss/vite";
 import { devtools } from "@tanstack/devtools-vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
-import { tanstackRouter } from '@tanstack/router-plugin/vite'
-import viteReact from "@vitejs/plugin-react";
+import { tanstackRouter } from "@tanstack/router-plugin/vite";
 import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
-import tsconfigPaths from "vite-tsconfig-paths";
+import viteReact from "@vitejs/plugin-react";
 
-const config = defineConfig({
-  ssr: {
-    noExternal: true,
-  },
-  build: {
-    chunkSizeWarningLimit: 1000,
-    sourcemap: false,
-    target: "esnext",
-    minify: "esbuild",
-    rollupOptions: {
-      treeshake: true,
-      output: {
-        manualChunks: undefined,
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd())
+  const isnoExternal = env.NODE_ENV = 'production'
+
+  return {
+    resolve: {
+      tsconfigPaths: true,
+    },
+
+    ssr: {
+      noExternal: isnoExternal,
+    },
+
+    build: {
+      chunkSizeWarningLimit: 1000,
+      sourcemap: false,
+      target: "esnext",
+      minify: "esbuild",
+      rollupOptions: {
+        treeshake: true,
       },
     },
-  },
-  define: {
-    global: "globalThis",
-  },
-  plugins: [
-    devtools(),
-    tsconfigPaths({ projects: ["./tsconfig.json"] }),
-    tailwindcss(),
-    tanstackStart(),
-    viteReact(),
-    react({
-      babel: {
-        plugins: ["babel-plugin-react-compiler"],
-      },
-    }),
-    tanstackRouter({
-      autoCodeSplitting: true,
-    }),
-  ],
-});
 
-export default config;
+    define: {
+      global: "globalThis",
+    },
+
+    plugins: [
+      devtools(),
+      tailwindcss(),
+      tanstackStart(),
+      viteReact(),
+
+      react(),
+      babel({
+        babelConfig: {
+          plugins: ['babel-plugin-react-compiler'],
+        },
+      }),
+
+      tanstackRouter({
+        autoCodeSplitting: true,
+      }),
+    ],
+  }
+})
