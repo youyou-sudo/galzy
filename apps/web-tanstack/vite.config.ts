e7f631fcf629e739d10e81eaf103303075615e25
小忧忧 <60727068+youyou-sudo@users.noreply.github.com>
2026-04-08 14:41:46 +0800

    fix: 更新 Vite 配置，添加 Babel 和 React 插件以支持新功能

--- apps/web-tanstack/vite.config.ts
+++ apps/web-tanstack/vite.config.ts
@@ -1,13 +1,16 @@
 import { defineConfig, loadEnv } from "vite";
+import babel from 'vite-plugin-babel';
 import tailwindcss from "@tailwindcss/vite";
 import { devtools } from "@tanstack/devtools-vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import { tanstackRouter } from "@tanstack/router-plugin/vite";
+import react from "@vitejs/plugin-react";
 import viteReact from "@vitejs/plugin-react";
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd())
   const isnoExternal = env.NODE_ENV = 'production'
+
   return {
     resolve: {
       tsconfigPaths: true,
@@ -36,6 +39,13 @@ export default defineConfig(({ mode }) => {
       tailwindcss(),
       tanstackStart(),
       viteReact(),
+
+      react(),
+      babel({
+        babelConfig: {
+          plugins: ['babel-plugin-react-compiler'],
+        },
+      }),
 
       tanstackRouter({
         autoCodeSplitting: true,
