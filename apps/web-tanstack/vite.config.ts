58ac6a437b7b671a2838b39229dcc979966e30bf
小忧忧 <60727068+youyou-sudo@users.noreply.github.com>
2026-04-08 14:42:42 +0800

    fix: 简化 Vite 配置，移除环境变量加载逻辑

--- apps/web-tanstack/vite.config.ts
+++ apps/web-tanstack/vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from "vite";
 import babel from 'vite-plugin-babel';
 import tailwindcss from "@tailwindcss/vite";
 import { devtools } from "@tanstack/devtools-vite";
@@ -7,49 +7,45 @@ import { tanstackRouter } from "@tanstack/router-plugin/vite";
 import react from "@vitejs/plugin-react";
 import viteReact from "@vitejs/plugin-react";
 
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd())
-  const isnoExternal = env.NODE_ENV = 'production'
+export default defineConfig({
+  resolve: {
+    tsconfigPaths: true,
+  },
 
-  return {
-    resolve: {
-      tsconfigPaths: true,
+  ssr: {
+    noExternal: true,
+  },
+
+  build: {
+    chunkSizeWarningLimit: 1000,
+    sourcemap: false,
+    target: "esnext",
+    minify: "esbuild",
+    rollupOptions: {
+      treeshake: true,
     },
+  },
 
-    ssr: {
-      noExternal: isnoExternal,
-    },
+  define: {
+    global: "globalThis",
+  },
 
-    build: {
-      chunkSizeWarningLimit: 1000,
-      sourcemap: false,
-      target: "esnext",
-      minify: "esbuild",
-      rollupOptions: {
-        treeshake: true,
+  plugins: [
+    devtools(),
+    tailwindcss(),
+    tanstackStart(),
+    viteReact(),
+
+    react(),
+    babel({
+      babelConfig: {
+        plugins: ['babel-plugin-react-compiler'],
       },
-    },
+    }),
 
-    define: {
-      global: "globalThis",
-    },
-
-    plugins: [
-      devtools(),
-      tailwindcss(),
-      tanstackStart(),
-      viteReact(),
-
-      react(),
-      babel({
-        babelConfig: {
-          plugins: ['babel-plugin-react-compiler'],
-        },
-      }),
-
-      tanstackRouter({
-        autoCodeSplitting: true,
-      }),
-    ],
-  }
-})
+    tanstackRouter({
+      autoCodeSplitting: true,
+    }),
+  ],
+}
+)
