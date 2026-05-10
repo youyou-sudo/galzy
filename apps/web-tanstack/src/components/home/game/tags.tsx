2d9d29706381e75e4fc82425f8d7b4bafd9527ee
小忧忧 <60727068+youyou-sudo@users.noreply.github.com>
2026-05-10 20:16:44 +0800

    fix: 修复 tag 显示问题

--- apps/web-tanstack/src/components/home/game/tags.tsx
+++ apps/web-tanstack/src/components/home/game/tags.tsx
@@ -14,18 +14,18 @@ export function TagsCard() {
 
 	return (
 		<div className="mt-4 mb-5">
-			<Accordion type="single" collapsible className="w-full ">
-				<AccordionItem value="tags" className="px-3 border rounded-lg">
-					<AccordionTrigger className="text-sm opacity-70 hover:opacity-100 py-3">
-						游戏标签
-					</AccordionTrigger>
-					<AccordionContent className="pb-3">
-						<Await
-							promise={tags}
-							fallback={<Skeleton className="w-full h-4" />}
-						>
-							{(tags) => {
-								return tags?.tags?.length === 0 ? null : (
+			<Await
+				promise={tags}
+				fallback={<Skeleton className="w-full h-10" />}
+			>
+				{(tags) => {
+					return tags?.tags?.length === 0 ? null : (
+						<Accordion type="single" collapsible className="w-full ">
+							<AccordionItem value="tags" className="px-3 border rounded-lg">
+								<AccordionTrigger className="text-sm opacity-70 hover:opacity-100 py-3">
+									游戏标签
+								</AccordionTrigger>
+								<AccordionContent className="pb-3">
 									<div className="flex flex-wrap gap-2">
 										{tags?.tags.map(
 											(item) =>
@@ -42,12 +42,12 @@ export function TagsCard() {
 												),
 										)}
 									</div>
-								);
-							}}
-						</Await>
-					</AccordionContent>
-				</AccordionItem>
-			</Accordion>
+								</AccordionContent>
+							</AccordionItem>
+						</Accordion>
+					);
+				}}
+			</Await>
 		</div>
 	);
 }
