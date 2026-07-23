c3bdd175043905961b06f56d7269fc9657d5616f
小忧忧 <60727068+youyou-sudo@users.noreply.github.com>
2026-07-23 11:31:36 +0800

    为 drizzle 配置添加 schema 支持

--- apps/api/src/db/client.ts
+++ apps/api/src/db/client.ts
@@ -1,6 +1,9 @@
 import { SQL } from 'bun'
 import { drizzle } from 'drizzle-orm/bun-sql'
+import * as schema from './schema'
 
 const client = new SQL(process.env.DATABASE_URL!)
-export const db = drizzle({ client })
+
+export const db = drizzle({ client, schema })
+
 export { sql } from 'drizzle-orm'
